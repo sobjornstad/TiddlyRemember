46a926d7cbf7e9a82db7929162c331e07bb067f9
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-05-09 22:43:58 -0500

    factor out several helper functions

--- src/twimport.py
+++ src/twimport.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 import subprocess
+from typing import Set
 
 from bs4 import BeautifulSoup
 
@@ -10,7 +11,7 @@ ZK_DIR = "/home/soren/cabinet/Me/Records/zettelkasten/zk-wiki"
 
 
 @dataclass
-class Question:
+class Note:
     id_: str
     tidref: str
     question: str
@@ -23,31 +24,59 @@ class Question:
         return hash(self.id_)
 
 
-outpt = subprocess.check_output(
-    ["/home/soren/cabinet/Me/Records/zettelkasten/node_modules/.bin/tiddlywiki",
-    "--output",
-    "/tmp/testdir",
-    "--render",
-    "[!is[system]type[text/vnd.tiddlywiki]]",
-    "[is[tiddler]addsuffix[.html]]",
-    "text/html",
-    "$:/sib/macros/remember"], cwd=ZK_DIR)
+def render_wiki(tw_binary: str, wiki_path: str, output_directory: str, filter_: str) -> None:
+    """
+    Request that TiddlyWiki render the specified tiddlers as html to a
+    location where we can inspect them for notes.
 
-results = set()
-for tiddler in Path("/tmp/testdir").glob("*.html"):
-    print(f"Processing {tiddler.name}...")
-    with open(tiddler, 'r') as f:
-        doc = f.read()
+    :param tw_binary: Path to the TiddlyWiki node executable.
+    :param wiki_path: Path of the wiki folder to render.
+    :param output_directory: Directory to render html files into.
+    :param filter_: TiddlyWiki filter describing which tiddlers we want
+                    to search for notes.
+    """
+    cmd = [
+        tw_binary,
+        "--output",
+        output_directory,
+        "--render",
+        filter_,
+        "[is[tiddler]addsuffix[.html]]",
+        "text/html",
+        "$:/sib/macros/remember"]
 
-    soup = BeautifulSoup(doc, 'html.parser')
+    #TODO: Error handling
+    subprocess.call(cmd, cwd=wiki_path)
+
+
+def notes_from_tiddler(tiddler: str, name: str) -> Set[Note]:
+    """
+    Given the text of a tiddler, parse the contents and return a set
+    containing all the Notes found within that tiddler.
+    """
+    notes = set()
+    soup = BeautifulSoup(tiddler, 'html.parser')
     pairs = soup.find_all("div", class_="rememberq")
     for pair in pairs:
         question = pair.find("div", class_="rquestion").p.get_text()
         answer = pair.find("div", class_="ranswer").p.get_text()
         id_ = pair.find("div", class_="rid").get_text().strip().lstrip('[').rstrip(']')
-        tidref = "tiddler"
-        tidref = tiddler.name[:tiddler.name.find('.html')]
+        notes.add(Note(id_, name, question, answer))
 
-        results.add(Question(id_, tidref, question, answer))
+    return notes
 
-print(results)
+render_wiki(
+    "/home/soren/cabinet/Me/Records/zettelkasten/node_modules/.bin/tiddlywiki",
+    ZK_DIR,
+    "/tmp/testdir",
+    "[!is[system]type[text/vnd.tiddlywiki]]")
+
+notes = set()
+for tiddler in Path("/tmp/testdir").glob("*.html"):
+    with open(tiddler, 'r') as f:
+        tid_text = f.read()
+    tid_name = tiddler.name[:tiddler.name.find('.html')]
+    print(f"Processing {tid_name}...")
+    notes.update(notes_from_tiddler(tid_text, tid_name))
+
+print(notes)
