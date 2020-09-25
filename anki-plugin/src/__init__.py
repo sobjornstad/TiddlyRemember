dfd145f1a7840dd4ec3ebe487dd4283a9db92055
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-09-25 16:13:56 -0500

    make ankisync take col directly, not mw
    This makes it possible to run a sync without the GUI running. We weren't
    using anything else on mw anyway.

--- anki-plugin/src/__init__.py
+++ anki-plugin/src/__init__.py
@@ -174,7 +174,7 @@ class ImportDialog(QDialog):
         """
         self.form.progressBar.setMaximum(0)
         self.form.text.setText("Applying note changes to your collection...")
-        userlog = ankisync.sync(self.notes, self.mw, self.conf)
+        userlog = ankisync.sync(self.notes, self.mw.col, self.conf)
 
         self.accept()
         self.mw.reset()
