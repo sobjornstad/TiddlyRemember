81a20287d0f2807e03dccd22b9dd7591dcca67f9
Soren I. Bjornstad <contact@sorenbjornstad.com>
2021-07-24 18:17:32 -0500

    bump versions and add changelogs

--- anki-plugin/src/util.py
+++ anki-plugin/src/util.py
@@ -9,8 +9,8 @@ from typing import NewType, Optional
 Twid = NewType('Twid', str)
 
 DEFAULT_FILTER = '[type[text/vnd.tiddlywiki]] [type[]] +[!is[system]]'
-PLUGIN_VERSION = "1.2.2"
-COMPATIBLE_TW_VERSIONS = ["", "1.2.2"]
+PLUGIN_VERSION = "1.2.3"
+COMPATIBLE_TW_VERSIONS = ["", "1.2.2", "1.2.3"]
 
 
 def pluralize(sg: str, n: int, pl: str = None) -> str:
