83666e4e3d1cb256543e3aeb9c167ef327ff444a
Soren I. Bjornstad <contact@sorenbjornstad.com>
2021-09-29 09:26:48 -0500

    support exporting scheduling information with macro calls

--- anki-plugin/src/macro_exporter.py
+++ anki-plugin/src/macro_exporter.py
@@ -4,8 +4,13 @@ macro_exporter.py - Export TiddlyRemember macros from compatible Anki notes
 
 from __future__ import annotations
 
-from anki.exporting import Exporter
+import datetime
+from typing import Optional
+
+import anki.consts
 from anki.collection import Collection
+from anki.exporting import Exporter
+from anki.notes import Note
 
 from . import twnote
 
@@ -17,6 +22,7 @@ class TiddlyRememberMacroExporter(Exporter):
     will be ignored.
     """
     ext = ".tid"
+    includeSched = False
 
     def __init__(self, col) -> None:
         Exporter.__init__(self, col)
@@ -25,6 +31,22 @@ class TiddlyRememberMacroExporter(Exporter):
     @staticmethod
     def key(col: Collection) -> str:
         return "TiddlyRemember notes"
+
+    def sched(self, note: Note) -> Optional[str]:
+        """
+        Given a note, return a 'sched' string representing its scheduling info
+        if it's a review card, or None otherwise.
+        """
+        c = note.cards()[0]
+
+        if c.type == anki.consts.CARD_TYPE_REV:
+            due_days_from_today = c.due - self.col.sched.today
+            due_date = (datetime.datetime.now().date()
+                        + datetime.timedelta(days=due_days_from_today))
+            due_str = due_date.strftime(r"%Y%m%d1200000")
+            return f"due:{due_str};ivl:{c.ivl};ease:{c.factor};lapses:{c.lapses}"
+        else:
+            return None
 
     def doExport(self, path) -> None:
         cids = sorted(self.cardIds())
@@ -37,7 +59,8 @@ class TiddlyRememberMacroExporter(Exporter):
             assert anki_note_type is not None, "Note type of existing note was None!"
             tw_note_type = twnote.by_name(anki_note_type['name'])
             if tw_note_type is not None:
-                out.append(tw_note_type.export_macro(n))
+                schedule_str = self.sched(n) if self.includeSched else ""
+                out.append(tw_note_type.export_macro(n, schedule_str))
 
         self.count = len(out)
         path.write('\n\n'.join(out).encode("utf-8"))
--- tests/test_export.py
+++ tests/test_export.py
@@ -23,50 +23,77 @@ from testutils import col_tuple, fn_params
 
 
 @pytest.mark.parametrize(
-    ("test_case_name", "output"),
+    ("test_case_name", "include_scheduling", "output"),
     [
-        ("BasicQuestionAndAnswer", """
+        # Basic types
+        ("BasicQuestionAndAnswer", False, """
             <<rememberq
                 "20200925171619043"
                 "What is TiddlyRemember good for?"
                 "Remembering things that you put in your TiddlyWiki.">>
             """),
-        ("BasicPair", """
+        ("BasicPair", False, """
             <<rememberp
                 "20200925223930460"
                 "One"
                 "1">>
             """),
-        ("EscapedCloze", r"""
+        ("EscapedCloze", False, r"""
             <<remembercz
                 "20210925133938745"
                 "In LaTeX, we create fractions with {c1::<code>\frac\{numerator\}\{denominator\}</code>}.">>
             """),
+
         # Hard references and other options aren't preserved.
-        ("HardrefQa", """
+        ("HardrefQa", False, """
             <<rememberq
                 "20200925183527082"
                 "Do hard references work in TiddlyRemember?"
                 "I don't know, you tell me!">>
             """),
+
         # Nothing special happens to media references, and the Anki name is preserved.
         # So if you want the images, you can drag and drop them out of
         # the media folder into a wiki to import them and everything should work.
-        ("InternalDogImageTest", """
+        ("InternalDogImageTest", False, """
             <<rememberq
                 "20210928004052765"
                 "What does a dog look like?"
                 '<img src="tr-f53cec5dc23d10d91500c50d79ccb4e73df697f64fc2cd93a1b2fcf2698775c5.jpg" width="300"/>'>>
             """),
-        ("AudioTest", '''
+        ("AudioTest", False, '''
             <<rememberq
                 "20210928205233830"
                 """What's this audio file?<br/><audio controls="controls" src="tr-07d11170fe30596ca17307682d2745e09862a8dcdf90c76fa90b70d381eb4973.mp3"></audio>"""
                 "A <em>test</em> audio file.">>
             '''),
+
+        # Scheduling can be included.
+        ("ScheduledQuestionAndAnswer", True, '''
+            <<rememberq
+                "20200925171619043"
+                "What is TiddlyRemember good for?"
+                "Remembering things that you put in your TiddlyWiki."
+                sched:"due:220009221200000;ivl:5;ease:1800;lapses:1">>
+            '''),
+        # But it isn't included even if it exists if you say not to.
+        ("ScheduledQuestionAndAnswer", False, '''
+            <<rememberq
+                "20200925171619043"
+                "What is TiddlyRemember good for?"
+                "Remembering things that you put in your TiddlyWiki.">>
+            '''),
+        # And if you say to but it's a new card, there won't be any.
+        ("BasicQuestionAndAnswer", True, """
+            <<rememberq
+                "20200925171619043"
+                "What is TiddlyRemember good for?"
+                "Remembering things that you put in your TiddlyWiki.">>
+            """),
     ]
 )
-def test_export(fn_params, col_tuple, tmp_path, test_case_name, output):
+def test_export(fn_params, col_tuple, tmp_path,
+                test_case_name, include_scheduling, output):
     "Check that we can sync an item into Anki and then export it as a TiddlyWiki macro."
     fn_params['filter_'] = test_case_name
     os.chdir(col_tuple.cwd)
@@ -74,6 +101,8 @@ def test_export(fn_params, col_tuple, tmp_path, test_case_name, output):
     sync(notes, col_tuple.col, "Default")
 
     exp = TiddlyRememberMacroExporter(col_tuple.col)
+    if include_scheduling:
+        exp.includeSched = True
     exp.exportInto(tmp_path / "output.txt")
     with open(tmp_path / "output.txt", "r") as f:  # pylint: disable=unspecified-encoding
         macros = f.read()
