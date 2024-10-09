b4303865d0ea6c101f3c6a993be3ee91e456fe80
Soren I. Bjornstad <contact@sorenbjornstad.com>
2024-10-09 08:21:33 -0500

    fix compatible versions metadata
    Resolves #82.

--- anki-plugin/src/util.py
+++ anki-plugin/src/util.py
@@ -11,9 +11,9 @@ from typing import Iterator, NewType, Optional, Sequence
 Twid = NewType('Twid', str)
 
 DEFAULT_FILTER = '[type[text/vnd.tiddlywiki]] [type[]] +[!is[system]]'
-PLUGIN_VERSION = "1.3.5"
+PLUGIN_VERSION = "1.3.6"
 COMPATIBLE_TW_VERSIONS = [
-    "", "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.3.4"
+    "", "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.3.2", "1.3.3", "1.3.4", "1.3.5", "1.3.6"
 ]
 
 
